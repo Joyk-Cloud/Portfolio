65fffe4a6f70f2c0c696b8a6159baca8c7e09999
Joyk-Cloud <jkasarachi2106@gmail.com>
2025-01-12 12:48:59 +0000

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Example: Form submission handler
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Message Sent! We will get back to you soon.");
+  });
+  
+  // Placeholder functions to add new projects or skills
+  function addNewProject() {
+    alert('You can add a new project manually in the HTML file.');
+  }
+  
+  function addNewSkill() {
+    alert('You can add a new skill manually in the HTML file.');
+  }
+
+  
